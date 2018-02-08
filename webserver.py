f32e9ae211c13ea30dced23c50735cbdb0624a80
Connor <cje5361@rit.edu>
2018-02-08 10:13:42 -0500

    First server working for normal browsers

--- webserver.py
+++ webserver.py
@@ -1,10 +1,36 @@
 import sys
 import socket
-import threading # To permit concurrent client connections
+import threading  # To permit concurrent client connections
+import json  # To read config files
 
 
-def requestHandler():
-    pass
+def getConfig(configfile):
+    """
+    Retrieve configuration from settings file
+    Params:
+        configfile: String containing config file name
+    Return:
+        Dictionary linking settings keywords to definitions
+    """
+    with open(configfile) as f:
+        data = json.load(f)
+    return data
+
+
+def requestHandler(client):
+    """
+    A thread used to parse and respond to a particular HTTP request.
+    Params:
+        client - a socket object representing the client.
+    Return:
+        None
+    """
+    print("Received: {}".format(client.recv(2048)))
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    body = "Hello World!"
+    client.send(response + body)
+    # Because HTTP is connectionless we close it at the end of every action
+    client.close()
 
 
 def main():
@@ -15,17 +41,15 @@ def main():
         None
     """
     try:
-        host = sys.argv[1]
-        port = int(sys.argv[2])
+        conf = getConfig(sys.argv[1])
     except IndexError:
-        print("usage: ./webserver.py <IP of interface> <port>")
-        print("defaulting to 0.0.0.0:8080.")
-        host = "0.0.0.0"
-        port = 80
+        print("usage: ./webserver.py <configfile>")
+        print("Defaulting to \"webserver.cfg\"")
+        conf = getConfig("webserver.cfg")
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Set up shop
-    s.bind((host, port))
+    s.bind((conf["host"], int(conf["port"])))
     s.listen(10)  # concurrent connections possible
 
     try:
@@ -33,15 +57,14 @@ def main():
             # Client is an object representing the client
             # adr is an array of information about the client
             client, adr = s.accept()  # Accept is a blocking call
-            print(adr[0], adr[1])
-    except:
-        s.close()
 
-    # example send data
-    # s.send("GET / HTTP/{0}\n\n".format(version))
+            # requestHandler is the function being run in the thread
+            # args are the parameters it takes
+            # Yes you need the dumb comma because it needs to be iterable
+            threading.Thread(target=requestHandler, args=(client,)).start()
 
-    # example get data
-    # result = s.recv(2048)
+    except socket.error, exc:
+        print("Caught exception socket.error : {}".format(exc))
 
 
 if __name__ == "__main__":
